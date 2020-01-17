7aa48af2cc636ed6aaeca6b301eb50d68c42c2cf
Fatma Khairy <fatmakhairy66@gmail.com>
2020-01-17 20:41:54 +0200

    new giver design

--- public/feed/js/custom.js
+++ public/feed/js/custom.js
@@ -1,21 +1,32 @@
 $(document).ready(function () {
     //click btn mySelf
-    $('.streedDis').css('display','none');
- $('#mySelf').on('click',function (e) {
-     e.preventDefault();
-     $('.streedDis').css('display','none');
-     $('#send').css('display','none');
-     $('#foodPlace').css('display','block');
+
+    var $govern = $('#Agovernorates');
+    var $region = $('#Aregions');
+
+
+    $('#a > input').each(function() {
+        if ($(this).val() == "") {
+            console.log('h')
+        }
+    });
+
+    if($govern.val() == null && $region.val() == null){
+        alert('h')
+    }
+
+
+    $('#mySelf').on('click',function (e) {
+         e.preventDefault();
+         $('#foodPlace').css('display','block');
 
  })
     //end btn mySelf
 
     //click btn notMe
     $('#notMe').on('click',function (e) {
-        e.preventDefault();
-        $('#foodPlace').css('display','none');
-        $('.streedDis').css('display','block');
-        $('#send').css('display','block');
+            e.preventDefault();
+
     })
     //end btn notMe
 });
--- resources/views/giver.blade.php
+++ resources/views/giver.blade.php
@@ -23,77 +23,111 @@
                <h3> <li class="active"> مطعم </li></h3>
             </ol>
         </section>
-        <section class="content">
-            <div class="row">
-                <div class="col-md-4">
-                    <button type="button" id="mySelf" class="form-control btn btn-primary active">التبرع بنفسك </button>
-                </div>
-                <div class="col-md-4">
-                    <button type="button"  id="notMe" class="form-control btn btn-info">التبرع بواسطه متطوع </button>
-                </div>
-            </div>
-            <div class="row justify-content-right">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">التبرع باطعام</div>
-
-                        <div class="card-body">
-                            <form method="POST" action="">
-                                @csrf
-                                <div class="form-group row">
-                                    <label  class="col-md-4 col-form-label text-md-right">المحافظه</label>
-                                    <div class="col-md-6">
-                                        <input   list="governorates"  class="form-control" name="governorate" value="{{old('governorate')}}" required  autofocus>
-                                        <datalist id="governorates">
-                                            <option value="Internet Explorer">
-                                            <option value="Firefox">
-                                            <option value="Chrome">
-                                            <option value="Opera">
-                                            <option value="Safari">
-                                        </datalist>
+           <section class="content">
+                <div class="row justify-content-right">
+                    {{--first form --}}
+                    <div class="col-md-6">
+                         <div class="card">
+                              <div class="card-header">التبرع بنفسك</div>
+                               <div class="card-body">
+                                      <form  action="" method="get" id="a">
+                                    @csrf
+                                    <div class="form-group row">
+                                        <label  class="col-md-4 col-form-label text-md-right">المحافظه</label>
+                                        <div class="col-md-6">
+                                            <input   list="governorates"  class="form-control" name="governorate" value="{{old('governorate')}}" required  autofocus>
+                                            <datalist id="Agovernorates">
+                                                <option value="Internet Explorer">
+                                                <option value="Firefox">
+                                                <option value="Chrome">
+                                                <option value="Opera">
+                                                <option value="Safari">
+                                            </datalist>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-md-4 col-form-label text-md-right">الحي</label>
-                                    <div class="col-md-6">
-                                        <input  list="regions" class="form-control" name="region" value="{{old('region')}}" required  autofocus>
-                                        <datalist id="regions">
-                                            <option value="Internet Explorer">
-                                            <option value="Firefox">
-                                            <option value="Chrome">
-                                            <option value="Opera">
-                                            <option value="Safari">
-                                        </datalist>
+                                    <div class="form-group row">
+                                        <label class="col-md-4 col-form-label text-md-right">الحي</label>
+                                        <div class="col-md-6">
+                                            <input  list="regions" class="form-control" name="region" value="{{old('region')}}" required  autofocus>
+                                            <datalist id="Aregions">
+                                                <option value="Internet Explorer">
+                                                <option value="Firefox">
+                                                <option value="Chrome">
+                                                <option value="Opera">
+                                                <option value="Safari">
+                                            </datalist>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-md-4 col-form-label text-md-right streedDis">العنوان التفصيل </label>
-                                    <div class="col-md-6">
-                                        <textarea  class="form-control streedDis" name="street"  required>
-                                        </textarea>
+                                    <div class="form-group row" style="display: none" id="foodPlace">
+                                        <label class="col-md-4 col-form-label text-md-right"> اماكن تواجد صندوق الطعام </label>
+                                        <div class="col-md-6">
+                                            <ul>
+                                                  <li><data value="place1">Cherry Tomato</data></li>
+                                                  <li><data value="place2">Beef Tomato</data></li>
+                                                  <li><data value="place3">Snack Tomato</data></li>
+                                            </ul>
+                                        </div>
                                     </div>
-                                </div>
-
-                                <div class="form-group row" style="display: none" id="foodPlace">
-                                    <label class="col-md-4 col-form-label text-md-right"> اماكن تواجد صندوق الطعام </label>
-                                    <div class="col-md-6">
-                                        <ul>
-                                              <li><data value="place1">Cherry Tomato</data></li>
-                                              <li><data value="place2">Beef Tomato</data></li>
-                                              <li><data value="place3">Snack Tomato</data></li>
-                                        </ul>
+                                    <div class="form-group row">
+                                        <div class="col-md-4">
+                                            <button type="submit" id="mySelf" class="form-control btn btn-primary disabled">التبرع بنفسك </button>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-6 offset-md-4">
-                                        <button type="submit" class="btn btn-primary" id="send" style="display: none">
-                                            {{ 'ارسال'}}
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
+                    {{--end first form --}}
+                    {{--secend form --}}
+                    <div class="col-md-6">
+                         <div class="card">
+                             <div class="card-header">التبرع بواسطه متطوع</div>
+                             <div class="card-body">
+                                        <form method="POST" action="">
+                                        @csrf
+                                        <div class="form-group row">
+                                            <label  class="col-md-4 col-form-label text-md-right">المحافظه</label>
+                                            <div class="col-md-6">
+                                                <input   list="governorates"  class="form-control" name="governorate" value="{{old('governorate')}}" required  autofocus>
+                                                <datalist id="governorates">
+                                                    <option value="Internet Explorer">
+                                                    <option value="Firefox">
+                                                    <option value="Chrome">
+                                                    <option value="Opera">
+                                                    <option value="Safari">
+                                                </datalist>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-4 col-form-label text-md-right">الحي</label>
+                                            <div class="col-md-6">
+                                                <input  list="regions" class="form-control" name="region" value="{{old('region')}}" required  autofocus>
+                                                <datalist id="regions">
+                                                    <option value="Internet Explorer">
+                                                    <option value="Firefox">
+                                                    <option value="Chrome">
+                                                    <option value="Opera">
+                                                    <option value="Safari">
+                                                </datalist>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-4 col-form-label text-md-right">العنوان التفصيل </label>
+                                            <div class="col-md-6">
+                                                <textarea  class="form-control" name="street" required autofocus>
+                                                </textarea>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-md-4">
+                                                <button type="submit" class="form-control btn btn-info" id="send">{{ 'ارسال'}} </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                  </div>
+                        </div>
+                    </div>
+                        {{--end sec form --}}
                 </div>
             </div>
         </section>
