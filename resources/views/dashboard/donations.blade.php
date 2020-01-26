74f180aaf0d2378d5834ad6d0f9ea1e03533d57a
Fatma Khairy <fatmakhairy66@gmail.com>
2020-01-26 13:29:34 +0200

    update

--- resources/views/dashboard/donations.blade.php
+++ resources/views/dashboard/donations.blade.php
@@ -98,20 +98,6 @@
             @endforeach
                 </tbody>
             </table>
-
-
-            {{--            <div class="clearfix">--}}
-            {{--                <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>--}}
-            {{--                <ul class="pagination">--}}
-            {{--                    <li class="page-item disabled"><a href="#"><i class="fa fa-angle-double-left"></i></a></li>--}}
-            {{--                    <li class="page-item"><a href="#" class="page-link">1</a></li>--}}
-            {{--                    <li class="page-item"><a href="#" class="page-link">2</a></li>--}}
-            {{--                    <li class="page-item active"><a href="#" class="page-link">3</a></li>--}}
-            {{--                    <li class="page-item"><a href="#" class="page-link">4</a></li>--}}
-            {{--                    <li class="page-item"><a href="#" class="page-link">5</a></li>--}}
-            {{--                    <li class="page-item"><a href="#" class="page-link"><i class="fa fa-angle-double-right"></i></a></li>--}}
-            {{--                </ul>--}}
-            {{--            </div>--}}
         </div>
     </div>
 @endsection
